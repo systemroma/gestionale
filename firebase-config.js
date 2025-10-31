dbb42f27625874455faf7837af17fce9380107f0
systemroma <system.roma@gmail.com>
2025-10-31 14:31:23 +0100

    Create firebase-config.js with Firebase settings
    Add Firebase configuration file with necessary keys

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// !!! INCOLLA QUI LA TUA CONFIGURAZIONE DA FIREBASE !!!
+// Questo file non verrà tracciato da Git (grazie a .gitignore)
+
+const firebaseConfig = {
+  apiKey: "AIza...",
+  authDomain: "tuo-progetto.firebaseapp.com",
+  projectId: "tuo-progetto",
+  storageBucket: "tuo-progetto.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:..."
+};
